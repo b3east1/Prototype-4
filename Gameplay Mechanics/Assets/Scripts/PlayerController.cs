de1acf5829193deb8130fffec2595a092874d88b
b3east1 <rhylendelarosa@gmail.com>
2023-11-15 11:48:43 -0800

    finished 4.3.6

--- Gameplay Mechanics/Assets/Scripts/PlayerController.cs
+++ Gameplay Mechanics/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
    
     public float speed = 5.0f;
     public bool hasPowerup = false;
+    public GameObject powerupIndicator;
 
 
     // Start is called before the first frame update
@@ -23,7 +24,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
+        
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,8 +35,17 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
         }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
